5d5ffb5eb09db16bed71fe29bc0d4fcda9c62cdc
zero-ljz <2267719005@qq.com>
2023-07-16 13:53:09 +0800

    同步调整Navicat Premium 16、服务器环境初始化脚本

--- batchfile/Navicat Premium 16.bat
+++ batchfile/Navicat Premium 16.bat
@@ -1,3 +1,3 @@
-regedit /s "C:\Users\LJZ\OneDrive\应用\Windows\Navicat Premium 16 重置试用.reg"
+regedit /s "C:\Users\LJZ\repos\scripts\batchfile\Navicat Premium 16 重置试用.reg"
 
 explorer "C:\Program Files\PremiumSoft\Navicat Premium 16\navicat.exe"
