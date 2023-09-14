158c934d6d5c8d3612ba225845b07d758b76e862
zero-ljz <2267719005@qq.com>
2023-09-14 15:31:03 +0800

    1

--- shell/backup.sh
+++ shell/backup.sh
@@ -8,7 +8,7 @@ apt -y install rsync sshpass
 save_dir="/backup"
 mkdir -p $save_dir
 
-mysql_root_password=$(cat /root/MYSQL_ROOT_PASSWORD.txt)
+mysql_root_password=$(cat ~/MYSQL_ROOT_PASSWORD.txt)
 
 remote_server=$1 # root@127.0.0.1
 remote_password=$2
@@ -20,14 +20,28 @@ backup_dirs=(
     "/var/www"
 )
 for dir in "${backup_dirs[@]}"; do
-    echo "备份目录: $dir"
     dir_name=$(basename "$dir")
     tar -czvf "${save_dir}/${dir_name}_dir_backup_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz" "$dir"
+    [ $? -ne 0 ] && echo "备份目录 $dir 失败" && exit 1
 done
 
-echo "备份MySQL容器中的所有数据库"
-docker exec -i mysql1 mysqldump -u root -p"$mysql_root_password" --all-databases > "$save_dir/mysql_backup_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
-[ $? -ne 0 ] && echo "MySQL数据库备份失败！" && exit 1
+# echo "备份MySQL容器中的所有数据库"
+# docker exec -i mysql1 mysqldump -u root -p"$mysql_root_password" --all-databases > "$save_dir/mysql_backup_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
+# [ $? -ne 0 ] && echo "MySQL数据库备份失败！" && exit 1
+
+
+
+# 获取所有数据库的列表
+databases=$(docker exec -i mysql1 mysql -u"root" -p"$mysql_root_password" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql)")
+
+# 备份每个数据库到单独的文件
+for db in $databases; do
+    backup_file="$backup_dir/${db}_backup_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
+    docker exec -i mysql1 mysqldump -u"root" -p"$mysql_root_password" --databases "$db" > "$backup_file"
+    [ $? -ne 0 ] && echo "备份数据库 $db 失败" && exit 1
+done
+
+
 
 # 备份所有正在运行的容器
 containers=$(docker ps -q)
@@ -42,7 +56,7 @@ for container_id in $containers; do
     docker commit "$container_name" "$backup_image"
     # 将镜像保存为压缩文件
     docker save -o "${save_dir}/$backup_file" "$backup_image"
-    [ $? -ne 0 ] && echo "${container_name}容器备份失败！" && exit 1
+    [ $? -ne 0 ] && echo "备份容器 ${container_name} 失败！" && exit 1
 done
 
 # 从保存的压缩文件中载入镜像
@@ -53,7 +67,7 @@ done
 echo "开始同步备份文件到远程服务器"
 # 如果不加-o StrictHostKeyChecking=no，就一定要先用ssh命令登录一次目标服务器，并输入yes将远程服务器地址永久添加到known hosts list（已知主机列表）
 # 使用rsync上传备份文件到远程服务器（使用密码认证）
-rsync -avvvz -e "/usr/bin/sshpass -p $remote_password ssh -o StrictHostKeyChecking=no" "$save_dir" "$remote_server:$remote_dir"
+#rsync -avvvz -e "/usr/bin/sshpass -p $remote_password ssh -o StrictHostKeyChecking=no" "$save_dir" "$remote_server:$remote_dir"
 [ $? -ne 0 ] && echo "备份文件上传失败！"
 
 
