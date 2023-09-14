9da1202086c68f331a836ed11f33d5bbcefd40b6
zero-ljz <2267719005@qq.com>
2023-09-14 14:13:41 +0800

    调整backup的backup_directories、backup_dirs逻辑

--- shell/backup.sh
+++ shell/backup.sh
@@ -4,7 +4,9 @@
 
 apt -y install rsync sshpass
 
-backup_dir="/backup" && mkdir -p $backup_dir
+save_dir="/backup"
+mkdir -p $save_dir
+
 mysql_root_password=$(cat /root/MYSQL_ROOT_PASSWORD.txt)
 
 remote_server=$1 # root@127.0.0.1
@@ -12,48 +14,41 @@ remote_password=$2
 remote_dir=${3:-"/"}
 
 # 指定要备份的文件目录
-backup_directories=(
+backup_dirs=(
     "/docker"
     "/var/www"
 )
-for dir in "${backup_directories[@]}"; do
+for dir in "${backup_dirs[@]}"; do
     dir_name=$(basename "$dir")
-    tar -czvf "${backup_dir}/${dir_name}_backup_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz" "$dir"
+    tar -czvf "${save_dir}/${dir_name}_dir_backup_$(date +\%Y\%m\%d_\%H\%M\%S).tar.gz" "$dir"
 done
 
 # 备份MySQL数据库
-docker exec -i mysql1 mysqldump -u root -p"$mysql_root_password" --all-databases > "$backup_dir/mysql_backup_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
+docker exec -i mysql1 mysqldump -u root -p"$mysql_root_password" --all-databases > "$save_dir/mysql_backup_$(date +\%Y\%m\%d_\%H\%M\%S).sql"
 [ $? -ne 0 ] && echo "MySQL数据库备份失败！" && exit 1
 
-
-# 容器备份 右边是镜像名
-# docker commit nginx1 nginx1
-# docker save -o nginx1_backup.tar nginx1
-
-# docker load -i nginx1_backup.tar
-# docker run -dp 80:80 --name nginx1 
-
+# 备份所有正在运行的容器
 containers=$(docker ps -q)
 for container_id in $containers; do
     container_name=$(docker inspect --format '{{.Name}}' $container_id | cut -c 2-)
-    backup_image="${container_name}-image-backup_$(date +\%Y\%m\%d_\%H\%M\%S)"
-    backup_file="${container_name}-image-backup_$(date +\%Y\%m\%d_\%H\%M\%S).tar"
+    backup_image="${container_name}-image"
+    backup_file="${container_name}_image_backup_$(date +\%Y\%m\%d_\%H\%M\%S).tar"
 
-    # 备份容器的镜像
+    docker rmi --force "$backup_image"
+    # 将容器保存为新镜像
     docker commit "$container_name" "$backup_image"
-    docker save -o "$backup_file" "$backup_image"
+    # 将镜像保存为压缩文件
+    docker save -o "${save_dir}/$backup_file" "$backup_image"
+    [ $? -ne 0 ] && echo "${container_name}容器备份失败！" && exit 1
 
-    # 保存容器镜像到备份目录
-    docker save -o "$backup_dir/$timestamp/container_image.tar" "$backup_dir/$timestamp/container_image"
-
-    echo "容器备份完成"
+# 从保存的压缩文件中载入镜像
+# docker load -i nginx1_backup.tar
+# docker run -dp 80:80 --name nginx1 
 
 
 # 如果不加-o StrictHostKeyChecking=no，就一定要先用ssh命令登录一次目标服务器，并输入yes将远程服务器地址永久添加到known hosts list（已知主机列表）
-# ssh root@host
-
 # 使用rsync上传备份文件到远程服务器（使用密码认证）
-rsync -avvvz -e "/usr/bin/sshpass -p $remote_password ssh -o StrictHostKeyChecking=no" "$backup_dir" "$remote_server:$remote_dir"
+rsync -avvvz -e "/usr/bin/sshpass -p $remote_password ssh -o StrictHostKeyChecking=no" "$save_dir" "$remote_server:$remote_dir"
 [ $? -ne 0 ] && echo "备份文件上传失败！"
 
 
