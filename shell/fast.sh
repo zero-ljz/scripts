67f99ac92e9aaaaeb84461085944466aae27d66d
zero-ljz <2267719005@qq.com>
2023-09-20 17:05:18 +0800

    1

--- shell/fast.sh
+++ shell/fast.sh
@@ -1401,31 +1401,29 @@ if [ "$1" = "-d" ] || [ "$1" = "--declare" ]; then declare -f ${FUNCNAME}; retur
 function auto_mode(){
 if [ "$1" = "-d" ] || [ "$1" = "--declare" ]; then declare -f ${FUNCNAME}; return; fi
     system_init
-
     install_supervisor
-    # install_utils
-
     install_docker
-    install_nodejs
-    install_phpfpm
-
-    # apt -y install nginx
-
-    install_filebrowser
-
+    install_frp
     # install_aria2
-    # install_frp
+    install_nodejs
 
     deploy_mysql
+    deploy_redis
     deploy_nginx
+    deploy_php_fpm
+    deploy_tinyfilemanager
+    deploy_adminer
+
+
+
+
+
+
     # deploy_debian
 
     # deploy_portainer
     # deploy_cloudreve
     # deploy_searxng
-    
-    # systemctl enable nginx
-    # systemctl restart nginx
     
 }
 
