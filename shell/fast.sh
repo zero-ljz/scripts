698dd2ba99e3d1425ab5190e385a9317fcbc1f58
zero-ljz <2267719005@qq.com>
2023-09-23 18:12:24 +0800

    更新服务器环境初始化脚本中的版本配置

--- shell/fast.sh
+++ shell/fast.sh
@@ -1423,7 +1423,7 @@ interpreter=${1:-"python3"}
 shift 1
 if [ "$interpreter" = "python3" ]; then
     command=${@:-"pip3 install -r requirements.txt && python3 app.py"}
-    docker run -it --rm --name py1 -v $PWD:/usr/src/myapp -w /usr/src/myapp python:3.9.13-slim-bullseye bash -c "${command}"
+    docker run -it --rm -p 8000:8000 -p 8080:8080 --name py1 -v $PWD:/usr/src/myapp -w /usr/src/myapp python:3.9.13-slim-bullseye bash -c "${command}"
 elif [ "$interpreter" = "python" ]; then
     command=${@:-"pip install -r requirements.txt && python app.py"}
     docker run -it --rm --name py1 -v $PWD:/usr/src/myapp -w /usr/src/myapp python:2.7.18-slim-buster ${command}
