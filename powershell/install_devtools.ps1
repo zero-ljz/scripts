cc7c331b50b33ef2c60757de0f9ca751b73d3ff5
zero-ljz <2267719005@qq.com>
2024-04-12 23:24:36 +0800

    更新浏览器工具脚本中的软件安装项

--- powershell/install_devtools.ps1
+++ powershell/install_devtools.ps1
@@ -6,11 +6,11 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
 # Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-winget install -e --id OpenJS.NodeJS.LTS
-winget install -e --id Python.Python.3.9 --version 3.9.13
+winget install -e --id OpenJS.NodeJS.LTS  --version 18.20.2
+winget install -e --id Python.Python.3.10 --version 3.10.11
 winget install -e --id Git.Git
 winget install -e --id GitHub.GitHubDesktop
-winget install -e --id Microsoft.WindowsTerminal
+# winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Microsoft.PowerToys
 winget install -e --id CosmoX.Lepton
 winget install -e --id AutoHotkey.AutoHotkey --version 1.1.37.00
@@ -33,6 +33,51 @@ winget install -e --id Anki.Anki
 winget install -e --id Xmind.Xmind
 
 winget install -e --id MariaDB.Server --version 10.4.30
+
+winget install -e --id VideoLAN.VLC
+winget install -e --id TeamViewer.TeamViewer
+winget install -e --id VMware.WorkstationPro
+winget install -e --id Notion.Notion
+winget install -e --id Obsidian.Obsidian
+winget install -e --id HTTPie.HTTPie
+winget install -e --id ElectronCommunity.ElectronFiddle
+winget install -e --id Cloudflare.Warp
+winget install -e --id Mubu.Mubu # 幕布
+winget install -e --id Automattic.Simplenote
+winget install -e --id Oracle.MySQLShell
+winget install -e --id Oracle.MySQLWorkbench
+winget install -e --id 7zip.7zip
+winget install -e --id Bandisoft.Bandizip --version 6.29
+winget install -e --id BlueStack.BlueStacks --version 5.20.0.1037
+
+winget install -e --id ByteDance.StreamingTool # 直播伴侣
+winget install -e --id ByteDance.Douyin
+winget install -e --id ByteDance.JianyingPro
+winget install -e --id Bilibili.Bilibili
+winget install -e --id CCTV.CBox
+
+winget install -e --id NetEase.CloudMusic # 网易云音乐
+winget install -e --id Youdao.YoudaoTranslate # 网易有道翻译
+
+winget install -e --id Baidu.BaiduNetdisk # 百度网盘
+winget install -e --id Baidu.BaiduTranslate # 百度翻译
+
+winget install -e --id Alibaba.DingTalk # 钉钉
+winget install -e --id Alibaba.aDrive # 阿里云盘
+
+winget install -e --id Tencent.TencentMeeting # 腾讯会议
+winget install -e --id Tencent.TencentVideo # 腾讯视频
+winget install -e --id Tencent.QQPlayer # QQ影音
+winget install -e --id Tencent.YingYongBao # 应用宝
+winget install -e --id Tencent.QQMusic
+winget install -e --id Tencent.QQ
+winget install -e --id Tencent.WeChat
+winget install -e --id Tencent.WeType # 微信输入法
+winget install -e --id Tencent.WeCom # 企业微信
+winget install -e --id Tencent.WeixinDevTools # 微信开发者工具
+
+
+
 
 
 # 使用 winget 安装 VSCode
