21bd74a20be9f43469d8130f2b3290531f45934e
zero-ljz <2267719005@qq.com>
2024-06-22 00:39:22 +0800

    同步调整浏览器工具脚本、wsl_init

--- shell/wsl_init.sh
+++ shell/wsl_init.sh
@@ -39,9 +39,9 @@ pipx ensurepath
 
 # 安装GUI应用
 # GNOME 文件管理器
-sudo apt install nautilus -y
+# sudo apt install nautilus -y
 # GNOME 文本编辑器
-sudo apt install gnome-text-editor -y
+# sudo apt install gnome-text-editor -y
 # Google Chrome
 wget https://p.520999.xyz/https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O /tmp/google-chrome-stable_current_amd64.deb
 sudo apt install --fix-missing /tmp/google-chrome-stable_current_amd64.deb
