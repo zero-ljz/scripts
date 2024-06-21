0b02c9a0a2e43d224078ef5781939e4d44f278aa
zero-ljz <2267719005@qq.com>
2024-06-22 00:39:22 +0800

    1

--- powershell/install_devtools.ps1
+++ powershell/install_devtools.ps1
@@ -1,11 +1,11 @@
 if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    curl -LkOJ https://p.iblog.site/https://github.com/microsoft/winget-cli/releases/download/v1.5.1881/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+    curl -LkOJ https://p.520999.xyz/https://github.com/microsoft/winget-cli/releases/download/v1.5.1881/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
     powershell -Command "Add-AppPackage -Path Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
 }
 
-winget install -e --id SomePythonThings.WingetUIStore
+# winget install -e --id SomePythonThings.WingetUIStore
 # winget install -e --id Microsoft.Teams.Free
-winget install -e --id Microsoft.WindowsPCHealthCheck
+# winget install -e --id Microsoft.WindowsPCHealthCheck
 # winget install -e --id Microsoft.MouseandKeyboardCenter
 winget install -e --id 9NBLGGH5R558 # To Do
 winget install -e --id 9MSPC6MP8FM4 # Whiteboard
@@ -17,7 +17,7 @@ winget install -e --id OpenJS.NodeJS.LTS  --version 18.20.2
 winget install -e --id Python.Python.3.10 --version 3.10.11
 winget install -e --id Git.Git
 winget install -e --id GitHub.GitHubDesktop
-winget install -e --id Microsoft.WindowsTerminal
+# winget install -e --id Microsoft.WindowsTerminal
 winget install -e --id Microsoft.PowerToys
 winget install -e --id Microsoft.BingWallpaper
 winget install -e --id CosmoX.Lepton
@@ -28,7 +28,7 @@ winget install -e --id Telegram.TelegramDesktop
 
 winget install -e --id Postman.Postman
 winget install -e --id Telerik.Fiddler.Classic
-winget install -e --id JavadMotallebi.NeatDownloadManager # 提示找不到包
+# winget install -e --id JavadMotallebi.NeatDownloadManager # 提示找不到包
 winget install -e --id WinSCP.WinSCP
 winget install -e --id c0re100.qBittorrent-Enhanced-Edition
 winget install -e --id voidtools.Everything
@@ -36,7 +36,7 @@ winget install -e --id SublimeHQ.SublimeText.4
 
 winget install -e --id PremiumSoft.NavicatPremium
 winget install -e --id AdrienAllard.FileConverter
-winget install -e --id DeepL.DeepL # 提示安装程序哈希不匹配: 以管理员身份运行时不能覆盖此内容
+# winget install -e --id DeepL.DeepL # 提示安装程序哈希不匹配: 以管理员身份运行时不能覆盖此内容
 winget install -e --id Anki.Anki
 winget install -e --id Xmind.Xmind
 
@@ -49,12 +49,12 @@ winget install -e --id VideoLAN.VLC
 winget install -e --id Notion.Notion
 winget install -e --id Obsidian.Obsidian
 winget install -e --id HTTPie.HTTPie
-winget install -e --id ElectronCommunity.ElectronFiddle # 刚安装完就提示有更新
+# winget install -e --id ElectronCommunity.ElectronFiddle # 刚安装完就提示有更新
 winget install -e --id Cloudflare.Warp
-winget install -e --id Mubu.Mubu # 幕布, 安装时提示执行InternetOpenUrl命令失败
+# winget install -e --id Mubu.Mubu # 幕布, 安装时提示执行InternetOpenUrl命令失败
 winget install -e --id Automattic.Simplenote
 winget install -e --id Joplin.Joplin
-winget install -e --id Dropbox.Dropbox
+# winget install -e --id Dropbox.Dropbox
 
 
 # winget install -e --id MariaDB.Server --version 10.6.17
@@ -70,7 +70,7 @@ winget install -e --id Bandisoft.Bandizip --version 6.29
 # winget install -e --id Bilibili.Bilibili
 # winget install -e --id CCTV.CBox
 
-# winget install -e --id NetEase.CloudMusic # 网易云音乐
+winget install -e --id NetEase.CloudMusic # 网易云音乐
 winget install -e --id Youdao.YoudaoTranslate # 网易有道翻译
 
 # winget install -e --id Baidu.BaiduNetdisk # 百度网盘, 包370m, 装完还添加了广告的开机自启
@@ -119,9 +119,9 @@ $vscodeExtensions = @(
 )
 
 # 命令执行失败
-foreach ($vscodeExtension in $vscodeExtensions) {
-    Start-Process -Wait -FilePath code -ArgumentList "--install-extension $vscodeExtension"
-}
+# foreach ($vscodeExtension in $vscodeExtensions) {
+#     Start-Process -Wait -FilePath code -ArgumentList "--install-extension $vscodeExtension"
+# }
 
 
 # 使用 winget 安装 Docker Desktop
