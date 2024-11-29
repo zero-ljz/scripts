0ffc75972e0458ba0800925ef3ef252939beeecd
zero-ljz <2267719005@qq.com>
2024-11-29 11:23:47 +0800

    更新 web_proxy.js

--- cfworkers/web_proxy.js
+++ cfworkers/web_proxy.js
@@ -19,146 +19,106 @@ async function handleRequest(request) {
 
   let newUrl = url.pathname.slice(1);
   if (queryParams) {
-    newUrl += "?" + queryParams;
+      newUrl += "?" + queryParams;
   }
 
   if (newUrl) {
-    if (!newUrl.startsWith("http://") && !newUrl.startsWith("https://")) {
-      newUrl = "http://" + newUrl;
-    }
-
-    try {
-      // 处理浏览器发来的请求头
-      const headers = new Headers(request.headers);
-      // 要删除的请求头字段列表
-      const headersToRemove = [
-        "Host",
-        "Accept-Encoding", // 不把浏览器支持的编码方式发送给代理网址
-      ];
-      // 使用 delete 方法删除指定的请求头字段
-      for (const header of headersToRemove) {
-        headers.delete(header);
-      }
-      // 添加缓存控制字段
-      headers.set("Cache-Control", "no-cache");
-
-      // 发起代理请求
-      const proxyResponse = await fetch(newUrl, {
-        method: request.method,
-        headers: headers,
-        body: request.body,
-        redirect: "manual", // 不要自动重定向
-      });
-
-      // 处理代理地址返回的响应头
-      const responseHeaders = new Headers(proxyResponse.headers);
-      // 设置允许的请求来源
-      responseHeaders.set("Access-Control-Allow-Origin", "*"); // 允许所有来源访问，可以根据需求进行修改
-      // 设置允许的请求方法
-      responseHeaders.set(
-        "Access-Control-Allow-Methods",
-        "GET, POST, PUT, DELETE, OPTIONS"
-      );
-
-      // 将代理响应的头部字段复制到响应对象
-      for (const [key, value] of proxyResponse.headers.entries()) {
-        if (
-          ![
-            "Content-Length",
-            "Content-Encoding",
-            "Transfer-Encoding",
-            "Cache-Control",
-          ].includes(key)
-        ) {
-          responseHeaders.set(key, value);
-        }
+      if (!newUrl.startsWith("http://") && !newUrl.startsWith("https://")) {
+          newUrl = "http://" + newUrl;
       }
 
-      if (proxyResponse.status >= 301 && proxyResponse.status <= 308) {
-        // 遇到重定向时重定向到代理地址
-        const redirectLocation = proxyResponse.headers.get("Location");
-        if (redirectLocation) {
-          responseHeaders.set("Location", url.origin + "/" + redirectLocation);
-        }
+      try {
+          // 处理浏览器发来的请求头
+          const headers = new Headers(request.headers);
+          // 要删除的请求头字段列表
+          const headersToRemove = [
+              "Host",
+              "Accept-Encoding", // 不把浏览器支持的编码方式发送给代理网址
+          ];
+          // 使用 delete 方法删除指定的请求头字段
+          for (const header of headersToRemove) {
+              headers.delete(header);
+          }
+          // 添加缓存控制字段
+          headers.set("Cache-Control", "no-cache");
+
+          // 发起代理请求
+          const proxyResponse = await fetch(newUrl, {
+              method: request.method,
+              headers: headers,
+              body: request.body,
+              redirect: "manual", // 不要自动重定向
+          });
+
+          // 处理代理地址返回的响应头
+          const responseHeaders = new Headers(proxyResponse.headers);
+          // 设置允许的请求来源
+          responseHeaders.set("Access-Control-Allow-Origin", "*"); // 允许所有来源访问，可以根据需求进行修改
+          // 设置允许的请求方法
+          responseHeaders.set(
+              "Access-Control-Allow-Methods",
+              "GET, POST, PUT, DELETE, OPTIONS"
+          );
+
+          // 将代理响应的头部字段复制到响应对象
+          for (const [key, value] of proxyResponse.headers.entries()) {
+              if (
+                  ![
+                      "Content-Length",
+                      "Content-Encoding",
+                      "Transfer-Encoding",
+                      "Cache-Control",
+                  ].includes(key)
+              ) {
+                  responseHeaders.set(key, value);
+              }
+          }
+
+          if (proxyResponse.status >= 301 && proxyResponse.status <= 308) {
+              // 遇到重定向时重定向到代理地址
+              const redirectLocation = proxyResponse.headers.get("Location");
+              if (redirectLocation) {
+                  responseHeaders.set("Location", url.origin + "/" + redirectLocation);
+              }
+          }
+
+          if (
+              proxyResponse.headers.get("Content-Length") &&
+              parseInt(proxyResponse.headers.get("Content-Length")) > 1048576
+          ) {
+              console.log("<1MB");
+              const readable = proxyResponse.body;
+              if (!readable) {
+                  return new Response("No readable body", { status: 500 });
+              }
+
+              const { readable: transformed, writable } = new TransformStream();
+              const writer = writable.getWriter();
+              const reader = readable.getReader();
+
+              pump(reader, writer).catch((err) => {
+                  console.error("Stream error:", err);
+                  writer.close();
+              });
+
+              return new Response(transformed, {
+                  status: proxyResponse.status,
+                  headers: responseHeaders,
+              });
+          } else {
+
+              // 直接返回响应内容
+              let responseBody = await proxyResponse.arrayBuffer();
+              return new Response(responseBody, {
+                  status: proxyResponse.status,
+                  headers: responseHeaders,
+              });
+          }
+      } catch (error) {
+          return new Response(`Error occurred: ${error}`, { status: 500 });
       }
-
-      if (
-        proxyResponse.headers.get("Content-Length") &&
-        parseInt(proxyResponse.headers.get("Content-Length")) > 1048576
-      ) {
-        console.log("大于1MB");
-        // 边收边传，通过生成器逐步返回响应内容
-        // const responseBody = proxyResponse.body
-        // return new Response(responseBody, {
-        //   status: proxyResponse.status,
-        //   headers: responseHeaders,
-        // })
-
-        // 边收边传
-        const { readable, writable } = new TransformStream();
-
-        // 开始传输数据
-        const writer = writable.getWriter();
-        const reader = proxyResponse.body.getReader();
-
-        // 使用 transform 方法将数据从 reader 转移到 writer
-        await pump(reader, writer);
-
-        // 返回经过转换的响应
-        return new Response(readable, {
-          status: proxyResponse.status,
-          headers: responseHeaders,
-        });
-      } else {
-        //     let newUrlObj = new URL(newUrl);
-        //     // const OLD_URL = newUrlObj.host;
-        //     // const NEW_URL = url.origin + '/' + newUrlObj.host;
-        //     const OLD_URL = newUrlObj.origin;
-        //     const NEW_URL = url.origin + '/' + newUrlObj.origin;
-
-        //     class AttributeRewriter {
-        //       constructor(attributeName) {
-        //         this.attributeName = attributeName;
-        //       }
-        //       element(element) {
-        //         const attribute = element.getAttribute(this.attributeName);
-        //         if (attribute) {
-        //           element.setAttribute(
-        //             this.attributeName,
-        //             attribute.replace(OLD_URL, NEW_URL)
-        //           );
-        //         }
-        //       }
-        //     }
-
-        //     const rewriter = new HTMLRewriter()
-        //     .on("a", new AttributeRewriter("href"))
-        //     .on("img", new AttributeRewriter("src"))
-        //     .on("script", new AttributeRewriter("src"))
-        //     .on("link", new AttributeRewriter("href"));
-
-        //   if(proxyResponse.headers.get('Content-Type').startsWith("text/html"))
-        //   {
-        //     let proxyResponse2 = await rewriter.transform(proxyResponse);
-        //     const transformedContent = await proxyResponse2.arrayBuffer();
-        //     return new Response(transformedContent, {
-        //         status: proxyResponse.status,
-        //         headers: responseHeaders,
-        //       })
-        //   }
-
-        // 直接返回响应内容
-        let responseBody = await proxyResponse.arrayBuffer();
-        return new Response(responseBody, {
-          status: proxyResponse.status,
-          headers: responseHeaders,
-        });
-      }
-    } catch (error) {
-      return new Response(`Error occurred: ${error}`, { status: 500 });
-    }
   } else {
-    const html = `
+      const html = `
 <!DOCTYPE html>
 <html>
 
@@ -169,8 +129,8 @@ async function handleRequest(request) {
 <body>
     <h2>Web Proxy</h2>
     <form onsubmit="event.preventDefault(); window.location.href = '/' + new FormData(this).get('url');">
-        <label style="display: block; margin: 1rem 0" for="url">输入要代理的页面或文件URL</label>
-        <input type="text" id="url" name="url" placeholder="url" value="http://" required />
+        <label style="display: block; margin: 1rem 0" for="url">URL:</label>
+        <input type="text" id="url" name="url" placeholder="Resource url for proxy access" value="http://" required />
         <input type="submit" value="GO" />
     </form>
 </body>
@@ -178,21 +138,26 @@ async function handleRequest(request) {
 </html>
       `;
 
-    return new Response(html, {
-      headers: {
-        "content-type": "text/html;charset=UTF-8",
-      },
-    });
+      return new Response(html, {
+          headers: {
+              "content-type": "text/html;charset=UTF-8",
+          },
+      });
   }
 }
 
-// 辅助函数：异步地将从一个 reader 读取的数据写入一个 writer
 async function pump(reader, writer) {
-  const { value, done } = await reader.read();
-  if (done) {
-    writer.close();
-    return;
+  try {
+      while (true) {
+          const { value, done } = await reader.read();
+          if (done) {
+              writer.close();
+              break;
+          }
+          await writer.write(value);
+      }
+  } catch (err) {
+      console.error("Error in pump:", err);
+      writer.close();
   }
-  await writer.write(value);
-  return pump(reader, writer);
 }
