5a37eff360c90258821aec710e8467fb12dc1788
zero-ljz <2267719005@qq.com>
2025-10-16 17:39:01 +0800

    完善sqlalchemy示例, 增加fastapi样板

--- python/example/sqlalchemy_example.py
+++ python/example/sqlalchemy_example.py
@@ -1,4 +1,5 @@
 import os, datetime
+from typing import Any, List, Dict, Tuple, Set
 from sqlalchemy import Integer, Float, Numeric, String, Text, Unicode, Boolean, DateTime, Date, Time
 from sqlalchemy import Table, Column, ForeignKey, UniqueConstraint
 from sqlalchemy.orm import relationship, declarative_base
@@ -33,6 +34,8 @@ class Follow(Base, BaseMixin):
 
 class Article(Base, BaseMixin):
     __tablename__ = 'article'
+    user_id = Column(Integer, ForeignKey('user.id'))
+    
     title = Column(String(255), nullable=False, comment="标题")
     content = Column(Text, nullable=False, comment="正文")
     
@@ -78,7 +81,77 @@ def get_db():
         db.close()
         
 from sqlalchemy import insert, delete, update, select, func, text, and_, or_, not_, desc, asc
+from sqlalchemy.orm import Session
 
+def execute_query_get_list(
+    db: Session,
+    stmt: Any,
+    sort_by: str | None = None,
+    page: int | None = None,
+    per_page: int | None = 10,
+) -> dict[str, Any]:
+    '''
+    stmt = (select(*Alarm.__table__.columns))
+    count, row_dicts  = execute_query_get_list(db, stmt, page=page, per_page=per_page).values()
+    '''
+    
+    out: dict[str, Any] = {}
+    # 排序
+    if sort_by:
+        for field in sort_by.split(","):
+            if ":" in field:
+                field_name, sort_order = field.split(":")
+                if sort_order.lower() == "desc":
+                    stmt = stmt.order_by(desc(field_name))
+                else:
+                    stmt = stmt.order_by(field_name)
+            else:
+                stmt = stmt.order_by(field)
+    # 分页
+    if page:
+        count_stmt = select(func.count()).select_from(stmt.alias())
+        out["count"] = db.execute(count_stmt).scalar() # 相当于.scalars().first()
+        stmt = stmt.offset((page - 1) * per_page).limit(per_page)
+
+    rows = db.execute(stmt).mappings().all() # 类型 List[RowMapping]
+    out["data"] = [dict(row) for row in rows]
+    return out
+
+
+# 更新操作
 with next(get_db()) as db:
-    user = db.query(User).first()
+    stmt = insert(User).values(username='user1', password='pass1')
+    # stmt = delete(User).where(User.username == 'user1')
+    # stmt = update(User).where(User.username == 'user1').values(username='user2')
+    db.execute(stmt)
+    db.commit()
+    
+# 单个查询
+with next(get_db()) as db:
+    # Core 方式
+    stmt = select(User).select_from(User).where(User.username == 'user1')
+    user: User = db.execute(stmt).first()[0] # 或 user = db.execute(stmt).scalars().one_or_none()
     print(user)
+    
+    # ORM 方式
+    user: User = db.query(User).filter(User.username == 'user1').first()
+    print(user)
+    
+    # 转为字典
+    user_dict: Dict[str, Any] = {k: v for k, v in user.__dict__.items() if not k.startswith('_sa_')}
+    print(user_dict)
+
+# 多个查询
+with next(get_db()) as db:
+    # Core 方式
+    stmt = select(User).select_from(User).where(and_(User.username == 'user1', User.password == 'pass1')).order_by(desc(User.id)).offset(0).limit(10)
+    users: List[User] = db.execute(stmt).scalars().all()
+    print(users)
+    
+    # ORM 方式
+    users: List[User] = db.query(User).filter(and_(User.username == 'user1', User.password == 'pass1')).order_by(desc(User.id)).offset(0).limit(10).all()
+    print(users)
+
+    # 转为字典列表
+    user_dicts: List[Dict[str, Any]] = [{k: v for k, v in obj.__dict__.items() if not k.startswith('_sa_')} for obj in users]
+    print(user_dicts)
