323cb1c5407eff45492cff7271ef7191c120ed57
zero-ljz <2267719005@qq.com>
2026-06-25 23:55:34 +0800

    1

---
+++ batchfile/stop_mariadb.bat
@@ -0,0 +1,21 @@
+@echo off
+chcp 65001 > nul
+
+net session >nul 2>&1
+if %errorLevel% neq 0 (
+
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%~dp0"
+
+net stop mariadb
+pause
